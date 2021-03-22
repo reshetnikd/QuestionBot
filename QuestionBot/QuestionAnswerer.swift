0cbf59027a1e114afa75c1411d4ef2af4f4624b0
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-22 11:23:03 +0200

    Add placeholder answer

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,6 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
         // TODO: Write a response
-        return "?"
+        return "I'm sorry, I don't understand the question."
     }
 }
