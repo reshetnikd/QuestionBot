17f2b6a9b5ff1554fa355c61e9f6f686c60adcd2
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-22 11:31:17 +0200

    Add answers to MyQuestionAnswerer

--- QuestionBot/QuestionAnswerer.swift
+++ QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,26 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response
-        return "I'm sorry, I don't understand the question."
+        
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion == "where are the cookies?" {
+            return "In the cookie jar!"
+        } else if lowerQuestion.hasPrefix("where") {
+            return "To the North!"
+        } else if lowerQuestion.hasPrefix("hello") {
+            return "Why, hello there"
+        } else {
+            
+            let defaultNumber = question.count % 3
+            
+            if defaultNumber == 0 {
+                return "That really depends"
+            } else if defaultNumber == 1 {
+                return "Ask me again tomorrow"
+            } else {
+                return "Why you're asking?"
+            }
+            
+        }
     }
 }
